c0b78b7543fccc74a0220de19033820c81fc74c8
Todd Storm <stormo@todds-air.lan>
2024-09-18 18:17:49 -0600

    three cards across for tasks

--- components/tasks/TaskList.tsx
+++ components/tasks/TaskList.tsx
@@ -162,7 +162,7 @@ export default function TaskList({
 
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 bg-gray-100 p-4 rounded-lg">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 bg-gray-100 p-4 rounded-lg">
         {tasks.map((task) => (
           <Card key={task.id} className="flex flex-col">
             <CardContent className="p-4 flex-grow flex flex-col">
