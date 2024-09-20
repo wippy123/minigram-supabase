0a9f657680ea63dc11c7c0510f6158875b53e4ef
Todd Storm <stormo@todds-air.lan>
2024-09-20 09:46:42 -0600

    profile container page themed

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -12,7 +12,11 @@ export default async function AccountSettingsPage() {
   } = await supabase.auth.getUser();
 
   if (!user) {
-    return <div>Loading...</div>;
+    return (
+      <div className="text-center text-gray-700 dark:text-gray-300">
+        Loading...
+      </div>
+    );
   }
 
   const { data: profileSettings } = await supabase
@@ -22,20 +26,28 @@ export default async function AccountSettingsPage() {
     .single();
 
   return (
-    <div className="container mx-auto mt-10 bg-white p-6 rounded-lg shadow-sm">
-      <h1 className="text-2xl font-bold mb-5">Account Settings</h1>
+    <div className="container mx-auto mt-10 bg-white dark:bg-gray-800 p-6 rounded-lg shadow-sm">
+      <h1 className="text-2xl font-bold mb-5 text-gray-900 dark:text-gray-100">
+        Profile
+      </h1>
       <div className="flex flex-col md:flex-row gap-8">
-        <div className="w-full md:w-1/2 border p-4 bg-white rounded-md shadow-sm">
-          <h2 className="text-xl font-semibold mb-4">Personal Information</h2>
+        <div className="w-full md:w-1/2 border border-gray-200 dark:border-gray-700 p-4 bg-white dark:bg-gray-800 rounded-md shadow-sm">
+          <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-200">
+            Personal Information
+          </h2>
           <ProfileSettingsForm initialData={profileSettings} />
         </div>
-        <div className="w-full md:w-1/2 border p-4 bg-white rounded-md shadow-sm">
-          <h2 className="text-xl font-semibold mb-4">Reset Password</h2>
+        <div className="w-full md:w-1/2 border border-gray-200 dark:border-gray-700 p-4 bg-white dark:bg-gray-800 rounded-md shadow-sm">
+          <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-200">
+            Reset Password
+          </h2>
           <ResetPasswordForm />
         </div>
       </div>
-      <div className="mt-10 bg-white p-4 rounded-md shadow-sm">
-        <h2 className="text-xl font-semibold mb-4">Subscription</h2>
+      <div className="mt-10 border border-gray-200 dark:border-gray-700 p-4 bg-white dark:bg-gray-800 rounded-md shadow-sm">
+        <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-200">
+          Subscription
+        </h2>
         <SubscriptionSection />
       </div>
     </div>
