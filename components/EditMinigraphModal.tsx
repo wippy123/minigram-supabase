bd19858f1b3f59ae08fb0ca5b70af3e9c50385d3
Todd Storm <stormo@todds-air.lan>
2024-10-16 21:17:32 -0600

    fix social media

--- components/EditMinigraphModal.tsx
+++ components/EditMinigraphModal.tsx
@@ -7,11 +7,10 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { FieldValues, useForm } from "react-hook-form";
+import { FieldValues, useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import {
-  Form,
   FormField,
   FormItem,
   FormLabel,
@@ -52,7 +51,7 @@ export default function EditMinigraphModal({
 }: EditMinigraphModalProps) {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const supabase = createClientComponentClient();
-
+  console.log("minigraph", { minigraph });
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -63,9 +62,12 @@ export default function EditMinigraphModal({
       instagram: minigraph.instagram,
       twitter: minigraph.twitter,
     },
+    mode: "onChange",
   });
 
-  const onSubmit = async (data: FormValues) => {
+  const onSubmit = async () => {
+    const data = form.getValues();
+    console.log(data);
     setIsSubmitting(true);
     try {
       const { error } = await supabase
@@ -99,98 +101,105 @@ export default function EditMinigraphModal({
         <DialogHeader>
           <DialogTitle>Edit Minigraph</DialogTitle>
         </DialogHeader>
-        <Form onSubmit={form.handleSubmit(onSubmit)}>
-          <form className="space-y-4">
-            <FormField
-              name="name"
-              register={form.register}
-              errors={form.formState.errors}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="name">Name</FormLabel>
-                  <FormControl>
-                    <Input id="name" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              name="purpose"
-              register={form.register}
-              errors={form.formState.errors}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="purpose">Purpose</FormLabel>
-                  <FormControl>
-                    <Textarea id="purpose" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              name="url"
-              register={form.register}
-              errors={form.formState.errors}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="url">URL</FormLabel>
-                  <FormControl>
-                    <Input id="url" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <div className="space-y-4 mt-4">
-              <FormLabel htmlFor="">Social Media Platforms</FormLabel>
-              <div className="flex space-x-4">
-                <FormField
-                  name="facebook"
-                  register={form.register}
-                  errors={form.formState.errors}
-                  render={({ field }: { field: FieldValues }) => (
-                    <div className="flex items-center space-x-2">
-                      <Checkbox id="facebook" {...field} />
-                      <FormLabel htmlFor="facebook">
-                        <Facebook size={20} />
-                      </FormLabel>
-                    </div>
-                  )}
-                />
+        <form className="space-y-4">
+          <FormField
+            name="name"
+            register={form.register}
+            errors={form.formState.errors}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="name">Name</FormLabel>
+                <FormControl>
+                  <Input id="name" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="purpose"
+            register={form.register}
+            errors={form.formState.errors}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="purpose">Purpose</FormLabel>
+                <FormControl>
+                  <Textarea id="purpose" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="url"
+            register={form.register}
+            errors={form.formState.errors}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="url">URL</FormLabel>
+                <FormControl>
+                  <Input id="url" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <div className="space-y-4 mt-4">
+            <FormLabel htmlFor="">Social Media Platforms</FormLabel>
+            <div className="flex space-x-4">
+              <Controller
+                name="facebook"
+                control={form.control}
+                render={({ field }) => (
+                  <div className="flex items-center space-x-2">
+                    <Checkbox
+                      id="facebook"
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                    <FormLabel htmlFor="facebook">
+                      <Facebook size={20} />
+                    </FormLabel>
+                  </div>
+                )}
+              />
 
-                <FormField
-                  name="instagram"
-                  register={form.register}
-                  errors={form.formState.errors}
-                  render={({ field }: { field: FieldValues }) => (
-                    <div className="flex items-center space-x-2">
-                      <Checkbox id="instagram" {...field} />
-                      <FormLabel htmlFor="instagram">
-                        <Instagram size={20} />
-                      </FormLabel>
-                    </div>
-                  )}
-                />
+              <Controller
+                name="instagram"
+                control={form.control}
+                render={({ field }) => (
+                  <div className="flex items-center space-x-2">
+                    <Checkbox
+                      id="instagram"
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                    <FormLabel htmlFor="instagram">
+                      <Instagram size={20} />
+                    </FormLabel>
+                  </div>
+                )}
+              />
 
-                <FormField
-                  name="twitter"
-                  register={form.register}
-                  errors={form.formState.errors}
-                  render={({ field }: { field: FieldValues }) => (
-                    <div className="flex items-center space-x-2">
-                      <Checkbox id="twitter" {...field} />
-                      <FormLabel htmlFor="twitter">
-                        <Twitter size={20} />
-                      </FormLabel>
-                    </div>
-                  )}
-                />
-              </div>
+              <Controller
+                name="twitter"
+                control={form.control}
+                render={({ field }) => (
+                  <div className="flex items-center space-x-2">
+                    <Checkbox
+                      id="twitter"
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                    <FormLabel htmlFor="twitter">
+                      <Twitter size={20} />
+                    </FormLabel>
+                  </div>
+                )}
+              />
             </div>
-            <Button type="submit" disabled={isSubmitting}>
-              {isSubmitting ? "Updating..." : "Update Minigraph"}
-            </Button>
-          </form>
-        </Form>
+          </div>
+          <Button type="submit" disabled={isSubmitting} onClick={onSubmit}>
+            {isSubmitting ? "Updating..." : "Update Minigraph"}
+          </Button>
+        </form>
       </DialogContent>
     </Dialog>
   );
