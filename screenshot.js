d80f76abc8164674a23b71cbae51bd42ad7782b4
Todd Storm <todd.w.storm@gmail.com>
2024-10-28 15:54:25 -0600

    clean up

--- screenshot.js
+++
@@ -1,8 +0,0 @@
-
-      var page = require('webpage').create();
-      page.viewportSize = { width: 1024, height: 768 };
-      page.open('https://www.golftown.com', function() {
-        page.render('/Users/stormo/projects/minigram-supabase/screenshot.png');
-        phantom.exit();
-      });
-    
