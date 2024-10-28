1d7acab400ef81c3f3e75ebed2afbe0e8c44df0e
Todd Storm <todd.w.storm@gmail.com>
2024-10-28 16:04:12 -0600

    further logging

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -39,7 +39,7 @@ export const updateSession = async (request: NextRequest) => {
     // https://supabase.com/docs/guides/auth/server-side/nextjs
     const user = await supabase.auth.getUser();
 
-    console.log("Middleware user", user);
+    console.log("Middleware user", {user, path:request.nextUrl.pathname, error: user.error});
     // protected routes
     if (request.nextUrl.pathname.startsWith("/protected") && user.error) {
       return NextResponse.redirect(new URL("/sign-in", request.url));
