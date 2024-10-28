fb63a061dc72e573fdab6e11675ebd145e12caa0
Todd Storm <todd.w.storm@gmail.com>
2024-10-28 17:04:02 -0600

    see if this works to fix screenshot

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: [process.env.NEXT_PUBLIC_SUPABASE_URL, 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com'].filter(Boolean).map(url => new URL(url).hostname),
   },
+   experimental: {
+    serverComponentsExternalPackages: ['@sparticuz/chromium'],
+  }
 }
 
 module.exports = nextConfig
